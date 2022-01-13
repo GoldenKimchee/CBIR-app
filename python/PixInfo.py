f568b245cbc40815177069e85112750f85ebce88
Rachel Kim <rachelkim8084@gmail.com>
2022-01-13 13:23:21 -0800

    wip fixing methods

--- python/PixInfo.py
+++ python/PixInfo.py
@@ -46,29 +46,27 @@ class PixInfo:
 		# Create a list of pixel data for each image and add it
 		# to a list.
 		for im in self.imageList[:]:
-				
-			#pixList = list(im.getdata())  # Rachel: not sure how the representation works with 
-			pixList = im.load()  					 # getdata() even after testing, so im using load()
-			CcBins, InBins = self.encode(pixList)
+			width, height = im.size
+
+			# Get histogram bins for each method.
+			CcBins, InBins = self.encode(im, width, height)
+   
 			self.colorCode.append(CcBins)
 			self.intenCode.append(InBins)
 						
 
 	# Bin function returns an array of bins for each 
 	# image, both Intensity and Color-Code methods.
-	def encode(self, pixlist):
+	def encode(self, im, width, height):
 			
 		# 2D array initilazation for bins, initialized
 		# to zero.
-		CcBins = [0]*64  # Stores histogram bins. index 0 -> bin 1, index 1 -> bin2, ... index 24 -> bin 25
-		InBins = [0]*25
+		CcBins = [0]*65  # 64 bins. index 0 -> total number of pixels in picture, index 1 -> bin 1, index 2 -> bin 2 ...
+		InBins = [0]*26  # 25 bins. Again, first index stores total pixels.
 		
-		InBins = self.intensity_method(pixlist, InBins)
-		CcBins = self.color_code_method(pixlist, CcBins)            
-		#your code
+		InBins = self.intensity_method(im, width, height, InBins)
+		CcBins = self.color_code_method(im, width, height, CcBins)            
 		
-		# Return the list of binary digits, one digit for each
-		# pixel.
 		return CcBins, InBins
 		
 
@@ -77,16 +75,21 @@ class PixInfo:
 	# 24-bit of RGB (8 bits for each color channel) color 
 	# intensities are transformed into a single 8-bit value. 
 	# There are 24 histogram bins.
-	def intensity_method(self, pix, InBins):
-		width, height = pix.size  
+	def intensity_method(self, im, width, height, InBins):
+		total_pixels = width * height
+		InBins[0] = total_pixels
+  
 		for y in range(height): # reads pixels left to right, top down (by each row).
 			for x in range(width): # This example code reads the RGB (red, green, blue) values 
-				r, g, b = pix[x, y]  # in every pixel of a 'x' pixel wide 'y' pixel tall image.	
+       
+				r, g, b = im.getpixel((x, y))  # in every pixel of a 'x' pixel wide 'y' pixel tall image.	
 				intensity = (0.299*r) + (0.587*g) + (0.114*b)
-				bin = intensity // 10  # Division rounds down to bin number.. in this case bins will range 0-24 (25 bins)
-				if bin == 25:  # last bin is 240 to 255, so bin of 24 and 25 will correspond to bin 24
-					bin = 24
+				bin = (intensity + 10) // 10  # Division rounds down to bin number.. in this case bins will range 0-24 (25 bins).
+    
+				if bin == 26:  # last bin is 240 to 255, so bin of 24 and 25 will 
+					bin = 25   # correspond to bin 24, BUT +1 since first index stores total pixels.
 				InBins[bin] += 1  # allocate pixel to corresponding bin
+    
 		return InBins
 		
 	
@@ -94,17 +97,22 @@ class PixInfo:
 	# 24-bit of RGB color intensities transformed into 6-bit color
 	# code from the first 2 bits of each of the three colors. 
 	# There are 64 histogram bins. 
-	def color_code_method(self, pix, CcBins):
-		width, height = pix.size  
+	def color_code_method(self, im, width, height, CcBins):
+		total_pixels = width * height
+		CcBins[0] = total_pixels 
+  
 		for y in range(height): 
 			for x in range(width):  
-				r, g, b = pix[x, y]  	
+       
+				r, g, b = im.getpixel((x, y))	
 				r = self.first_two_nums( self.decimal_to_binary(r) )  # get binary representation, then 
 				g = self.first_two_nums( self.decimal_to_binary(g) )  # get the first two significant numbers
 				b = self.first_two_nums( self.decimal_to_binary(b) )
+    
 				color_code = r + g + b
 				bin = self.binary_to_decimal(color_code)
-				CcBins[bin] += 1  # allocate pixel to corresponding bin
+				CcBins[bin + 1] += 1  # allocate pixel to corresponding bin, +1 since first index stores total pixels
+    
 		return CcBins
 
 
